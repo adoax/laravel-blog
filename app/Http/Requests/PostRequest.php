b48457e3c25fbb340f9901123de5237c1a62453c
seguinard <anthony.seguinard@live.fr>
2020-02-18 11:55:38 +0100

    feat: ckeditor
    Ajout de ckeditor, pour le champ content dans les ports + gestion de l'upload image qui lui est liée

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -26,7 +26,6 @@ class PostRequest extends FormRequest
         return [
             'title' => ['required'],
             'content' => ['required'],
-            'image' => ['required']
         ];
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,27 +6,36 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
+window.ClassicEditor = require('@ckeditor/ckeditor5-build-classic');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+ClassicEditor
+    .create(document.querySelector('#editor'), {
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList',   "insertTable", '|', 'imageUpload',  ],
+        language: 'fr',
+        cloudServices: {
+            tokenUrl: 'https://64331.cke-cs.com/token/dev/9GaBukhQ2jnW1rbWp5by4KW2knkf4VZlX8Nr8duh2DRieWXEs29eG4uSmyrq',
+            uploadUrl: 'https://64331.cke-cs.com/easyimage/upload/'
+        },
+        image: {
+            // You need to configure the image toolbar, too, so it uses the new style buttons.
+            toolbar: [ 'imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight', ],
+            resizeUnit: 'px',
+            styles: [
+                // This option is equal to a situation where no style is applied.
+                'full',
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+                // This represents an image aligned to the left.
+                'alignLeft',
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+                // This represents an image aligned to the right.
+                'alignRight'
+            ]
+        }
+    })
+    .then( editor => {
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
